d637d11ba9c8471aef4d6f1e7a286d863cdc47fd
Ritik Pandey <128275361+TechyRitikInnovates@users.noreply.github.com>
2023-12-31 20:23:17 +0530

    pc

--- main.js
+++ main.js
@@ -172,4 +172,35 @@ function checkScreenWidth() {
 checkScreenWidth();
 
 // Listen for window resize events
-window.addEventListener('resize', checkScreenWidth);
+window.addEventListener('resize', checkScreenWidth); 
+
+const gap = 16;
+
+const carousel = document.getElementById("carousel"),
+  content = document.getElementById("content"),
+  next = document.getElementById("next"),
+  prev = document.getElementById("prev");
+
+next.addEventListener("click", e => {
+  carousel.scrollBy(width + gap, 0);
+  if (carousel.scrollWidth !== 0) {
+    prev.style.display = "flex";
+  }
+  if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+    next.style.display = "none";
+  }
+});
+prev.addEventListener("click", e => {
+  carousel.scrollBy(-(width + gap), 0);
+  if (carousel.scrollLeft - width - gap <= 0) {
+    prev.style.display = "none";
+  }
+  if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+    next.style.display = "flex";
+  }
+});
+
+let width = carousel.offsetWidth;
+window.addEventListener("resize", e => (width = carousel.offsetWidth));
+
+
